2b02187a97a7dd1ab6ac7fa2dfbc34f841c7520b
alisa <alisavy2010@gmail.com>
2022-11-30 22:24:46 +0300

    Initial Commit

---
+++ CourseWork3/InputFieldComponent.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  agvyshegorodtsevaPW2
+//
+//  Created by Алиса Вышегородцева on 28.09.2022.
+//
+
+import UIKit
+
+
+final class InputFieldView: UITextField {
+    
+    
+}
