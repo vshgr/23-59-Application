a5682e5cae442b8882b51eebc09bfe107ee26d97
Yana Barbashina <70941492+yanawishnya@users.noreply.github.com>
2022-12-05 19:40:10 +0300

    add close keyboard after clicking outside

---
+++ CourseWork3/frontend/UIViewController+Keyboard.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Keyboard.swift
+//  CourseWork3
+//
+//  Created by Барбашина Яна on 05.12.2022.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
