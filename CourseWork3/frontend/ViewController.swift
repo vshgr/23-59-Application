1b2b125ea0031a1ded4e8ee2caf6049734510fde
alisa <alisavy2010@gmail.com>
2022-12-05 15:49:05 +0300

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- CourseWork3/frontend/ViewController.swift
+++ CourseWork3/frontend/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let onloardingIconView = UIImageView(image: UIImage(named: "onboarding_logo"))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = UIColor(named: "mainColor")
+        pinLogo()
+    }
+    
+    private func pinLogo() {
+        let parent = self.view!
+        parent.addSubview(onloardingIconView)
+        onloardingIconView.center = CGPoint(x: parent.frame.size.width / 2,
+                                     y: parent.frame.size.height / 2 - parent.frame.size.height / 12 )
     }
 
 
