81de7b5f793ac2290ba5d8469a1b135ae8b51254
alisa <alisavy2010@gmail.com>
2022-12-05 15:21:56 +0300

    Merge remote-tracking branch 'refs/remotes/origin/main'

---
+++ CourseWork3/frontend/Components.swift
@@ -0,0 +1,18 @@
+//
+//  Components.swift
+//  CourseWork3
+//
+//  Created by Алиса Вышегородцева on 05.12.2022.
+//
+
+import Foundation
+import UIKit
+
+class Components {
+    let mainColor = UIColor(red: 0.809, green: 1, blue: 0.267, alpha: 1).cgColor
+    let hintColor = UIColor(red: 0.721, green: 0.721, blue: 0.721, alpha: 1)
+    let textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+    let attentionColor = UIColor(red: 0.913, green: 0.209, blue: 0.209, alpha: 1)
+    let doneColor = UIColor(red: 0.565, green: 0.824, blue: 0.012, alpha: 1).cgColor
+    let additionColor = UIColor(red: 0.44, green: 0, blue: 1, alpha: 1)
+}
