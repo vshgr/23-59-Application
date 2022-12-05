8df22a38ab0589661cd25c5415b2100742f1c73a
Yana Barbashina <70941492+yanawishnya@users.noreply.github.com>
2022-12-05 19:09:33 +0300

    code input view added

---
+++ CourseWork3/frontend/CodeInputViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CodeInputViewController.swift
+//  CourseWork3
+//
+//  Created by Барбашина Яна on 05.12.2022.
+//
+
+import UIKit
+
+class CodeInputViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+}
--- CourseWork3/frontend/EmailViewController.swift
+++ CourseWork3/frontend/EmailViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 class EmailViewController: UIViewController {
     private let emailLabel = UILabel()
     private let emailInput = UITextField()
+    private let codeButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         setupTextFeatures()
         setupInputEmail()
+        setupSendCodeButton()
     }
     
     private func setupView() {
@@ -45,5 +47,28 @@ class EmailViewController: UIViewController {
         
         let grid = Grid(view: self.view)
         emailInput.pin(to: self.view, [.left: grid.pin, .bottom: 461])
+    }
+    
+    private func setupSendCodeButton() {
+        let grid = Grid(view: self.view)
+        
+        codeButton.setTitle("Sign in", for: .normal)
+        codeButton.setTitleColor(UIColor(named: "white"), for: .normal)
+        codeButton.titleLabel?.font = UIFont(name: "FONTSPRINGDEMO-PontiacBoldRegular", size: 18)
+        codeButton.backgroundColor = UIColor(named: "black")
+        self.view.addSubview(codeButton)
+        
+        codeButton.setHeight(to: view.frame.size.height * 0.08)
+        codeButton.layer.cornerRadius = view.frame.size.height * 0.08 / 2
+        
+        codeButton.pin(to: self.view, [.left: grid.margin * 2, .right: grid.margin * 2, .bottom: grid.margin * 2])
+        
+        codeButton.addTarget(self, action: #selector(sendCodeButtonPressed), for: .touchUpInside)
+    }
+    
+    @objc
+    private func sendCodeButtonPressed() {
+        let codeInputController = CodeInputViewController()
+        navigationController?.pushViewController(codeInputController, animated: true)
     }
 }
